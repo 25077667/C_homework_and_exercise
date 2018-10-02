4daa158c5722961b662c85be3c32c36af8ba5acb
25077667 <zxc25077667@gmail.com>
2018-10-02 20:11:56 +0800

    input&output

---
+++ exercise/input&output/cpp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int in;
+    while(cin>>in)
+        cout<<sqrt((double)((in*3)/2)*((in*3)/2)*((in*3)/2))<<endl;
+    return 0;
+}
+/*
+int a = 0xE2;
+    int b = 0xC3;
+    int c = 0x0A;
+    int d = 0x08;
+    int  t = a^b^c^d; //23
+    cout<< hex<< t <<endl;
+*/
