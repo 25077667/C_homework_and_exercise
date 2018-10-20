91e44ffcbe13edc8377f5e766b03b8fd340c14fc
25077667 <zxc25077667@gmail.com>
2018-10-21 00:55:50 +0800

    error commit

--- homework/5/method 2.cpp
+++ homework/5/method 2.cpp
@@ -9,12 +9,12 @@ int original_function(int a,int b,int c,int d,int e,int x){
 int main()
 {
     int a,b,c,d,e,lower_x,upper_x;
-    cout<<"請輸入5個係數 : ";
+//    cout<<"請輸入5個係數 : ";
     cin>>a>>b>>c>>d>>e;
-    cout<<"請輸入x軸範圍 : ";
+//    cout<<"請輸入x軸範圍 : ";
     int cache1,cache2;  cin>>cache1>>cache2;    upper_x=max(cache1,cache2);lower_x=min(cache1,cache2);
-    int tan_point;  cout<<"請輸入所求切點的x值 : ";cin>>tan_point;if(tan_point>upper_x || tan_point<lower_x){cout<<"i want to crash ";system("pause");return 0;}
-    printf("實際切線斜率 : %.6f\n",tan_value(a,b,c,d,e,tan_point));
+    int tan_point;  cout<<" ";cin>>tan_point;if(tan_point>upper_x || tan_point<lower_x){cout<<"i want to crash ";system("pause");return 0;}
+    printf(": %.6f\n",tan_value(a,b,c,d,e,tan_point));
 
     cout<<"   -35  -30  -25  -20  -15  -10  -5    0     5    10   15   20   25   30   35\n";
     for(int x=lower_x; x<=upper_x; x++){
