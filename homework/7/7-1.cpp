9ad0feebf5b4d47494033959304a97176d908c05
25077667 <zxc25077667@gmail.com>
2018-11-01 23:29:30 +0800

    homework7

---
+++ homework/7/7-1.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+int blank=0;
+int check_range(int n){
+    if(n>0 && n<27)
+        return 1;
+    else
+        return 0;
+}
+
+void draw_parallelogram(int n){
+    if(blank == n)
+        return;
+    for(int i=0;i<blank;i++)
+        cout<<" ";
+    for(int i=0;i<n*2-1;i++){
+        if(i<n)
+            cout<<(char)('A'+i)<<" ";
+        else
+            cout<<(char)('A'+(2*(n-1)-i))<<" ";
+    }
+    blank++;cout<<endl;
+    draw_parallelogram(n);
+}
+
+void draw_diamond(int n){
+    int horizon=1,vertical=n;
+    char element = 'A';
+    for(int i=1;i<2*(n+1);i++){
+        char element = 'A';
+        for(int j=1;j<vertical;j++)
+            cout<<"  ";
+        for(int j=1;j<=horizon;j++){
+            cout<<element<<" ";
+            if(j<=horizon/2) element++;
+            else element--;
+        }
+        cout<<endl;
+        if(i>=n){
+            horizon-=2;
+            vertical++;
+        }
+        else{
+            horizon+=2;
+            vertical--;
+        }
+    }
+}
+int main(){
+    int n;
+    while(1){
+        blank =0;
+        cout<<"Please input the number N:";cin>>n;
+        if(!check_range(n)){
+            cout<<"The number N is out of range. Please input again!!\n"<<endl;
+            continue;
+        }
+        cout<<endl;
+        draw_parallelogram(n);
+        cout<<endl;
+        draw_diamond(n);
+    }
+    return 0;
+}
---
+++ homework/7/7-2.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include <time.h>
+using namespace std;
+int lower_char[26]={0},captal_char[26]={0};
+void read_write_file(){
+    //write file
+    char buff[1000];
+    srand(1);
+    FILE *file_pointer;
+    file_pointer = fopen("input1.txt","w+");
+    for(int i=0;i<1000;i++){
+        if(rand()%2)
+            buff[i]='A'+rand()%26;
+        else
+            buff[i]='a'+rand()%26;
+    }
+    fwrite(buff,1,1000,file_pointer);
+    fclose(file_pointer);
+    memset(buff,0,1000);//free the buff
+    //read file
+    char c;
+    file_pointer = fopen("input1.txt","r");
+    while(fscanf(file_pointer,"%c",&c)!=EOF){
+        if((int)c>96)
+            lower_char[(int)c -97]++;
+        else
+            captal_char[(int)c -65]++;
+    }
+    fclose(file_pointer);
+}
+int main(){
+    read_write_file();
+    while(1){
+        char c;
+        cout<<"請輸入要尋找的字元:";
+        cin>>c;
+
+        if((int)c>96 && (int) c<123)
+            printf("字元\'%c\'一共尋找到:%d",c,lower_char[(int)c-97]);
+        else if((int)c>64 && (int)c<91)
+            printf("字元\'%c\'一共尋找到:%d",c,captal_char[(int)c-65]);
+        else if(c == '*'){
+            cout<<"結束尋找。";
+            break;
+        }
+        else
+            cout<<"輸入錯誤，再試一次。";
+        cout<<endl<<endl;
+    }
+    return 0;
+}
