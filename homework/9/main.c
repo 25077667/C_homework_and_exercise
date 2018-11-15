46aad2b38d5ad6803becd2f624a6b2bdc5565538
25077667 <zxc25077667@gmail.com>
2018-11-16 00:08:42 +0800

    finish

---
+++ homework/9/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int sum=0;
+int mysubtotal(int first, int end_of_num){
+    if(first<end_of_num)
+        sum = first+mysubtotal(first+1,end_of_num);
+    else if (first == end_of_num)
+        return first;
+    else
+        return sum;
+}
+void myswap(int *input1, int *input2){
+    if(*input1> *input2)
+        (*input1)^=(*input2)^=(*input1)^=(*input2);
+    printf("The summation %d to %d is:%d \n",*input1,*input2, mysubtotal(*input1, *input2));
+}
+
+int main(){
+    int input1,input2;
+    sum=0;
+    if(scanf("%d %d",&input1,&input2) == EOF)
+        return 0;
+    myswap(&input1,&input2);
+    main();
+}
