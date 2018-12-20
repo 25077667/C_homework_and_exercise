b36805a6ce952adfb6d17a7dab54bd7344db34b1
25077667 <zxc25077667@gmail.com>
2018-12-20 10:44:01 +0800

    clear

--- homework/13/main/main.c
+++ homework/13/main/main.c
@@ -2,21 +2,20 @@
 
 int main()
 {
-    int arr[100000],i,tmp,want;
+    int arr[100000],i,tmp,want,times;
     FILE *file_pointer;
     file_pointer = fopen("input.txt","r");
 
-    for(i=0;fscanf(file_pointer," %d",&tmp)!=EOF;i++)
-        arr[i]=tmp;
+    fscanf(file_pointer,"%d",&times);
+    for(i=0;i<times;i++)
+        fscanf(file_pointer,"%d ",&arr[i]);
 
     mySort(arr,i);
-    //for(int j=0;j<i;j++)
-      //   printf("%d ",arr[j]);
     printf("Sorted finish.\n");
 
     while( printf("Please input your target:") && scanf("%d",&want)!=EOF){
         int index = myBinarySearch(arr,want,0,i);
-        printf("%d %d %d\n",arr[1550],arr[1551],arr[1552]);
+        //printf("%d %d %d\n",arr[1550],arr[1551],arr[1552]);
         if(index == -1)
             printf("target not found.\n");
         else
