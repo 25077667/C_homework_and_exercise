3fdf00ebaec172c2ae026cea3aeb281a9d1dae00
25077667 <zxc25077667@gmail.com>
2019-06-17 17:55:43 +0800

    clean code

--- homework/03/B073040047-1.cpp
+++ homework/03/B073040047-1.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int salary;
     string s = "Your annual salary is $";
-    while(1)
-    {
-        cout<<"Enter the value of monthly salary : ";
-        cin>>salary;
-        cout<<endl;
+    while (1) {
+        cout << "Enter the value of monthly salary : ";
+        cin >> salary;
+        cout << endl;
 
-        if(salary<30001)
-            cout<<s<<salary*13.5<<endl;
-        else if(salary<50001)
-            cout<<s<<salary*14<<endl;
-        else if(salary<70001)
-            cout<<s<<salary*14.5<<endl;
-        else if(salary>70000)
-            cout<<s<<salary*16<<endl;
+        if (salary < 30001)
+            cout << s << salary * 13.5 << endl;
+        else if (salary < 50001)
+            cout << s << salary * 14 << endl;
+        else if (salary < 70001)
+            cout << s << salary * 14.5 << endl;
+        else if (salary > 70000)
+            cout << s << salary * 16 << endl;
         else
-            continue; //²z½×¤W³o¦æ¤£·|°õ¦æ¨ì
+            continue;  //ï¿½zï¿½×¤Wï¿½oï¿½æ¤£ï¿½|ï¿½ï¿½ï¿½ï¿½ï¿½
     }
     return 0;
 }
