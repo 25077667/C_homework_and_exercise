918bf467ae471eb8fb0a2394b292e9a7e44c369f
25077667 <zxc25077667@gmail.com>
2019-06-17 17:59:53 +0800

    clean code

--- homework/12/main.cpp
+++ homework/12/main.cpp
@@ -1,43 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <cstdlib>
 
-struct stack{
+struct stack {
     int value;
-    struct stack *next;
+    struct stack* next;
 };
 
 typedef struct stack Stack;
 Stack* top = NULL;
 
-void show_stack(bool isempty, int value, bool ispush){
-    if(isempty && !ispush){
+void show_stack(bool isempty, int value, bool ispush) {
+    if (isempty && !ispush) {
         printf("Error pop.\nNothing in the stack.\n");
         return;
     }
 
-    if(ispush)  printf("push %d into stack.\n",value);
-    else    printf("pop %d from stack.\n",value);
+    if (ispush)
+        printf("push %d into stack.\n", value);
+    else
+        printf("pop %d from stack.\n", value);
 
     Stack* current;
     current = top;
-    while(current!=NULL){
+    while (current != NULL) {
         printf("*\t*\n");
-        printf("*%5d  *\n",current->value);
+        printf("*%5d  *\n", current->value);
         printf("*\t*\n");
         printf("*********\n");
         current = current->next;
     }
 }
 
-bool isempty(){
-    if(top== NULL)  return true;
-    else    return false;
+bool isempty() {
+    if (top == NULL)
+        return true;
+    else
+        return false;
 }
 
-void mypush(int input){
+void mypush(int input) {
     Stack* new_stack;
     new_stack = (Stack*)malloc(sizeof(Stack));
-    if(new_stack == NULL){
+    if (new_stack == NULL) {
         printf("memory dump\n");
         return;
     }
@@ -45,13 +49,14 @@ void mypush(int input){
     new_stack->value = input;
     new_stack->next = top;
     top = new_stack;
-    show_stack(isempty(), input,1);
+    show_stack(isempty(), input, 1);
 }
 
-void mypop(){
-    int top_value, now_is_empty=isempty();
-    if(!now_is_empty){
-        Stack* next_stack;Stack* preverous_stack;
+void mypop() {
+    int top_value, now_is_empty = isempty();
+    if (!now_is_empty) {
+        Stack* next_stack;
+        Stack* preverous_stack;
         preverous_stack = top;
         //switch the top of stack
         next_stack = top->next;
@@ -59,20 +64,19 @@ void mypop(){
         top = next_stack;
         free(preverous_stack);
     }
-    show_stack(now_is_empty,top_value,0);
+    show_stack(now_is_empty, top_value, 0);
 }
 
 int main() {
-    int choice,value;
+    int choice, value;
     printf("Please input the instruction:");
-    while((scanf("%d",&choice)!=EOF)){
-        if(choice == 1){
-            scanf("%d",&value);
+    while ((scanf("%d", &choice) != EOF)) {
+        if (choice == 1) {
+            scanf("%d", &value);
             mypush(value);
-        }
-        else
+        } else
             mypop();
         printf("Please input the instruction:");
     }
-	return 0;
+    return 0;
 }
