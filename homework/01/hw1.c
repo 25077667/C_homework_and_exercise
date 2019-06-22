70bc595560fca045bbbd593fa6a9d82b3092fccf
25077667 <zxc25077667@gmail.com>
2019-06-22 14:35:06 +0800

    clean

--- homework/01/hw1.c
+++ homework/01/hw1.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
+int main() {
     char c;
     scanf("%c", &c);
-        //printf("%c", c);
-        printf("      %c\n", c);
-        printf("     %c%c%c\n", c,c,c);
-        printf("  %c%c%c%c%c%c%c%c%c\n", c,c,c,c,c,c,c,c,c);
-        printf("    %c%c%c%c%c\n", c,c,c,c,c);
-        printf("  %c%c%c%c%c%c%c%c%c\n", c,c,c,c,c,c,c,c,c);
-        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c\n", c,c,c,c,c,c,c,c,c,c,c,c,c);
-        printf("    %c%c%c%c%c\n", c,c,c,c,c);
-        printf("    %c%c%c%c%c\n", c,c,c,c,c);
+    printf("      %c\n", c);
+    printf("     %c%c%c\n", c, c, c);
+    printf("  %c%c%c%c%c%c%c%c%c\n", c, c, c, c, c, c, c, c, c);
+    printf("    %c%c%c%c%c\n", c, c, c, c, c);
+    printf("  %c%c%c%c%c%c%c%c%c\n", c, c, c, c, c, c, c, c, c);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c\n", c, c, c, c, c, c, c, c, c, c, c, c, c);
+    printf("    %c%c%c%c%c\n", c, c, c, c, c);
+    printf("    %c%c%c%c%c\n", c, c, c, c, c);
     return 0;
 }
