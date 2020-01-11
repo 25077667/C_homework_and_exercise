a6d9b77fead9ae4d4e0e72bdff862c498dc7d7bf
25077667 <zxc25077667@gmail.com>
2020-01-11 16:26:14 +0800

    clear p6 code

--- homework/06/main.cpp
+++ homework/06/main.cpp
@@ -11,34 +11,35 @@ void leap_year_judge() {
     }
 }
 
+bool checkInput(int input) {
+    return input / 1000000 && !(input / 10000000) && (input % 1000) / 10 < 13 && input % 10 < 7;
+}
+
 int main() {
     leap_year_judge();
     int input;
-    while (cout << "Input: " && cin >> input && input / 1000000 && !(input / 10000000) && (input % 1000) / 10 < 13 && input % 10 < 7) {
+    while (cout << "Input: " && cin >> input && checkInput(input)) {
         int count_of_first_line = 7 - (input % 10);  //record the first line last number
         int count_of_looping = 1;
         if (years[input / 1000])
             mon[2] = 29;
         const char* line1[] = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         printf("%29s, %d\n", line1[(input % 1000) / 10], input / 1000);
-        cout << "  SUN  MON  TUE  WED  THU  FRI  SAT" << endl;
+        printf("  SUN  MON  TUE  WED  THU  FRI  SAT\n");
 
         for (int i = 0; i < input % 10; i++)
-            cout << "     ";
+            printf("     ");
         for (int i = input % 10, j = 1; i < 7; i++, j++)
             printf("%5d", j);
-        cout << endl;
-        for (int i = count_of_first_line + 1; i <= mon[(input % 1000) / 10]; i++) {
-            if (count_of_looping == 7) {
-                printf("%5d\n", i);
-                count_of_looping = 1;
-            } else {
-                printf("%5d", i);
-                count_of_looping++;
-            }
+        printf("\n");
+        for (int i = count_of_first_line + 1; i <= mon[(input % 1000) / 10]; i++, count_of_looping++) {
+            printf("%5d", i);
+            if (count_of_looping % 7 == 0)
+                printf("\n");
         }
-        cout << endl;
+        printf("\n");
     }
-    cout << "INPUT ERROR AND BREAK" << endl;
+    if (checkInput(input))
+        printf("INPUT ERROR AND BREAK");
     return 0;
 }
